db2d9287b249221a1f61db71b3bab1b3c71b15d2
Noor Ul Huda <noorulhuda3833@gmail.com>
2026-08-05 17:57:57 +0500

    Added workspace model and user associations

---
+++ app/models/workspace.rb
@@ -0,0 +1,6 @@
+class Workspace < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :user_id }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_03_125433) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_05_124608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,4 +21,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_03_125433) do
     t.string "password_digest"
     t.datetime "updated_at", null: false
   end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_workspaces_on_user_id"
+  end
+
+  add_foreign_key "workspaces", "users"
 end
