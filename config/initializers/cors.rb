dbf9ddec67f5cd5668565fced2ccb258e747f304
Noor Ul Huda <noorulhuda3833@gmail.com>
2026-09-22 18:04:08 +0500

    Allow current Vercel preview origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      "http://localhost:3001",
-      "http://127.0.0.1:3001",
-      "https://agency-os-frontend.vercel.app",
-      "https://agency-os-frontend-b4gff7h81-noorulhuda3833-7673s-projects.vercel.app"
-    )
+  "http://localhost:3001",
+  "http://127.0.0.1:3001",
+  "https://agency-os-frontend.vercel.app",
+  "https://agency-os-frontend-d0yfytqzd-noorulhuda3833-7673s-projects.vercel.app"
+)
 
     resource "*",
       headers: :any,
